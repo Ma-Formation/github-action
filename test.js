01b0a3f584b007030aa83ff132776bff063f8155
root <root@DESKTOP-PRECIEUX>
2024-10-15 22:09:57 +0200

    add build test

---
+++ test.js
@@ -0,0 +1,13 @@
+const greet = require("./index.js")
+console.log ("Running the tests");
+
+if (greet() === "Hello, World"){
+    console.log("Test Passed");
+    process.exit(0);
+} 
+else
+
+{
+    console.log ("Test failed");
+    process.exit(1);
+}
