7cf41be5907c783a1d41ead8a868a0ff96edc29e
root <root@DESKTOP-PRECIEUX>
2024-10-16 20:50:48 +0200

    fix ami main

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2-action" {
-  ami           = "ami-0c55b159cbfafe1f0"  # AMI Amazon Linux, changer selon la région
+  ami           = "ami-0866a3c8686eaeeba"  # AMI Amazon Linux, changer selon la région
   instance_type = "t2.micro"
 
   tags = {
