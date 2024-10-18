e2bb3c378da4e055d3dc48fb79d3530503402794
root <root@DESKTOP-PRECIEUX>
2024-10-18 21:45:00 +0200

    Add -b realeases and respect condition

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const greet = require("./index.js")
 console.log ("Running the tests");
 
 if (greet() === "Hello, World"){
-    console.log("Test Passed");
+    console.log("Test Passed !");
     process.exit(0);
 } 
 else
